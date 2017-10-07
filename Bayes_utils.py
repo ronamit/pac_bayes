a34d19697c09762e1196c6353f52e7bdb8b7b5dc
ronamit <amitron5@gmail.com>
2017-10-07 17:30:27 +0300

    updates

--- Bayes_utils.py
+++ Bayes_utils.py
@@ -1,15 +1,11 @@
 
 from __future__ import absolute_import, division, print_function
 
-import timeit
 import data_gen
 
 import numpy as np
 import torch
 from torch.autograd import Variable
-import torch.nn.functional as F
-
-import random
 import common as cmn
 from common import count_correct, get_param_from_model, grad_step
 from models_standard import get_model
