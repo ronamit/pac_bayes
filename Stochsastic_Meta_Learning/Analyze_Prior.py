2d048a9e6a5fc6ed06dc332a7f1f6a743bcc6ceb
ronamit <amitron5@gmail.com>
2017-10-08 20:54:22 +0300

    updates

--- Stochsastic_Meta_Learning/Analyze_Prior.py
+++ Stochsastic_Meta_Learning/Analyze_Prior.py
@@ -46,50 +46,68 @@ prior_model = models_Bayes.get_bayes_model(model_type, prm)
 # Then load the weights:
 load_model_state(prior_model, dir_path, name=f_name)
 
+def get_params_statistics(model, name1, name2):
+    param_list = [named_param for named_param in model.named_parameters() if name1 in named_param[0] and name2 in named_param[0]]
+    n_list = len(param_list)
+    mean_list = np.zeros(n_list)
+    std_list = np.zeros(n_list)
+    for i_param, named_param in enumerate(param_list):
+        param_name = named_param[0]
+        param_vals = named_param[1]
+        param_mean = param_vals.mean().data[0]
+        param_std = param_vals.std().data[0]
+        mean_list[i_param] = param_mean
+        std_list[i_param] = param_std
+        print('Parameter name: {}, mean value: {}, STD: {}'.format(param_name, param_mean, param_std))
 
-log_var_params = [named_param for named_param in prior_model.named_parameters() if 'log_var' in named_param[0]]
+    plot_statistics(mean_list, std_list, name1, name2)
+    return mean_list, std_list
 
-b_log_var = [named_param for named_param in log_var_params if '.b_' in named_param[0]]
-w_log_var = [named_param for named_param in log_var_params if '.w_' in named_param[0]]
-
-n_layers = len(b_log_var)
-b_mean = np.zeros(n_layers)
-b_std = np.zeros(n_layers)
-for i_param, named_param in enumerate(b_log_var):
-    param_name = named_param[0]
-    param_vals = named_param[1]
-    param_mean =  param_vals.mean().data[0]
-    param_std = param_vals.std().data[0]
-    b_mean[i_param] = param_mean
-    b_std[i_param] = param_std
-    print('Parameter name: {}, mean value: {}, STD: {}'.format(param_name, param_mean, param_std))
-
-w_mean = np.zeros(n_layers)
-w_std = np.zeros(n_layers)
-for i_param, named_param in enumerate(w_log_var):
-    param_name = named_param[0]
-    param_vals = named_param[1]
-    param_mean =  param_vals.mean().data[0]
-    param_std = param_vals.std().data[0]
-    w_mean[i_param] = param_mean
-    w_std[i_param] = param_std
-    print('Parameter name: {}, mean value: {}, STD: {}'.format(param_name, param_mean, param_std))
+def plot_statistics(mean_list, std_list, name1, name2):
+    plt.figure()
+    n_list = len(mean_list)
+    plt.errorbar(range(n_list), mean_list, yerr=std_list)
+    plt.title("Statistics of the prior {} - {}".format(name1, name2))
+    plt.xticks(np.arange(n_list))
+    plt.xlabel('Layer')
+    plt.ylabel('value')
 
 
-plt.figure()
-plt.errorbar(range(n_layers), w_mean, yerr=w_std)
-plt.title("Statistics of the prior log-var of the multiplication weights")
-plt.xticks(np.arange(n_layers))
-plt.xlabel('Layer')
-plt.ylabel('log-var param.')
 
-
-plt.figure()
-plt.errorbar(range(n_layers), b_mean, yerr=b_std)
-plt.title("Statistics of the prior log-var of the bias weights")
-plt.xticks(np.arange(n_layers))
-plt.xlabel('Layer')
-plt.ylabel('log-var param.')
-
+log_var_w_mean, log_var_w_srd = get_params_statistics(prior_model, '_log_var', '.w_')
+mu_w_mean, mu_w_srd = get_params_statistics(prior_model, '_mean', '.w_')
 
 plt.show()
+
+# log_var_params = [named_param for named_param in prior_model.named_parameters() if 'log_var' in named_param[0]]
+#
+# b_log_var = [named_param for named_param in log_var_params if '.b_' in named_param[0]]
+# w_log_var = [named_param for named_param in log_var_params if '.w_' in named_param[0]]
+#
+# n_layers = len(b_log_var)
+# b_log_var_mean = np.zeros(n_layers)
+# b_log_var_std = np.zeros(n_layers)
+#
+#
+#
+# w_mean = np.zeros(n_layers)
+# w_std = np.zeros(n_layers)
+# for i_param, named_param in enumerate(w_log_var):
+#     param_name = named_param[0]
+#     param_vals = named_param[1]
+#     param_mean =  param_vals.mean().data[0]
+#     param_std = param_vals.std().data[0]
+#     w_mean[i_param] = param_mean
+#     w_std[i_param] = param_std
+#     print('Parameter name: {}, mean value: {}, STD: {}'.format(param_name, param_mean, param_std))
+#
+#
+#
+# plt.figure()
+# plt.errorbar(range(n_layers), b_log_var_mean, yerr=b_log_var_std)
+# plt.title("Statistics of the prior log-var of the bias weights")
+# plt.xticks(np.arange(n_layers))
+# plt.xlabel('Layer')
+# plt.ylabel('log-var param.')
+
+
